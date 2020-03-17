588c32b1410abdafde74b12691a28733a998c55c
Eddie Madrigal <edmadrigal@yahoo.com>
2020-03-16 21:24:07 -0700

    refactored App.js

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { useReducer } from 'react';
 import { initialState, reducer } from './reducer';
 import './App.css';
 
-import TodoList from './components/TodoList.js';
-import TodoForm from './components/TodoForm.js';
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
